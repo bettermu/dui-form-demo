8c29e1bd9f1d631f90785d90a030ca9a5ffd1d3e
bettermu <1113783908@qq.com>
2022-03-13 10:42:30 +0800

    提交代码

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,68 @@
 <template>
   <div id="app">
-    <dui-form>
+    <!-- <dui-form>
       <dui-form-item :label="label">
         <el-input></el-input>
       </dui-form-item>
-    </dui-form>
-
-    <el-form-comp :formData="formData"></el-form-comp>
-
+    </dui-form> -->
+    <el-form-comp :formData="formData" >
+      <template #default="slot">
+        <div >
+          <el-button type="primary" @click="submitForm(slot.form)">提交</el-button>
+          <el-button @click="addItem">新增表单项</el-button>
+        </div>
+      </template>
+    </el-form-comp>
   </div>
 </template>
 
 <script>
-import DuiForm from './components/Form.vue'
-import DuiFormItem from './components/FormItem.vue'
-import ElFormComp from './components/elForm.vue'
-import formData from './config/config'
+import DuiForm from "./components/Form.vue";
+import DuiFormItem from "./components/FormItem.vue";
+import ElFormComp from "./components/elForm.vue";
+import formData from "./config/config";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     DuiForm,
     DuiFormItem,
-    ElFormComp
+    ElFormComp,
   },
-  data(){
+  data() {
     return {
-      label:'hhh',
-      formData:formData
+      label: "hhh",
+      formData: formData,
+    };
+  },
+  methods: {
+    addItem() {
+      this.formData.push({
+        name: "desc",
+        comp: "el-input",
+        compProps: {
+          type: "textarea",
+        },
+        itemProps: {
+          label: "活动形式",
+          prop: "desc",
+        },
+        value: "",
+        rules: [{ required: true, message: "请填写活动形式", trigger: "blur" }],
+      });
+    },
+    submitForm(ref){
+      ref.validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
     }
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss">
--- src/components/elForm.vue
+++ src/components/elForm.vue
@@ -1,8 +1,7 @@
 
 <template>
-
-<div>
-  <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+  <div>
+    <!-- <el-form :model="ruleForm" :rules="rules"  label-width="100px" class="demo-ruleForm">
   <el-form-item label="活动名称" prop="name">
     <el-input v-model="ruleForm.name"></el-input>
   </el-form-item>
@@ -49,97 +48,216 @@
     <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
     <el-button @click="resetForm('ruleForm')">重置</el-button>
   </el-form-item>
-</el-form>
+</el-form> -->
 
-<el-form label-width="100px" :model="formKey" :rules="formRules">
-  <el-form-item v-for="(item,index) in formData" :key="index" v-bind="item.itemProps"> 
-    <component :is='item.comp' v-model="item.value" v-bind="item.compProps">
-      <slot v-if="item.children" >
-        <component v-for="(it,ind) in item.children" :key="ind" :is='it.comp' v-bind="it.compProps"></component>
-      </slot>
-    </component>
-  </el-form-item>
-</el-form>
-</div>
-  
+    <el-form
+      label-width="100px"
+      ref="ruleForm"
+      :model="formKey"
+      :rules="formRules"
+    >
+      <el-form-item
+        v-for="(value, key, index) in visableFormData.properties"
+        :key="index"
+        v-bind="value.itemProps"
+      >
+        <component
+          :is="value.comp"
+          v-model="value.value"
+          v-bind="value.compProps"
+          v-on="makeEventCallback(value.eventObj, value.linkage)"
+        >
+          <template v-if="value.children">
+            <component
+              v-for="(it, ind) in value.children"
+              :key="ind"
+              :is="it.comp"
+              v-bind="it.compProps"
+            ></component>
+          </template>
+        </component>
+      </el-form-item>
+      <!-- 提交表单slot -->
+      <el-form-item v-if="hasSlot">
+        <slot :form="formRef"></slot>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
-  export default {
-    props:{
-      formData:{
-        type:Array,
-        default:()=>[]
-      }
+export default {
+  props: {
+    formData: {
+      type: Object,
+      default: () => {},
     },
-    data() {
-      return {
-        ruleForm: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        },
-        rules: {
-          name: [
-            { required: true, message: '请输入活动名称', trigger: 'blur' },
-            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-          ],
-          region: [
-            { required: true, message: '请选择活动区域', trigger: 'change' }
-          ],
-          date1: [
-            { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
-          ],
-          date2: [
-            { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
-          ],
-          type: [
-            { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
-          ],
-          resource: [
-            { required: true, message: '请选择活动资源', trigger: 'change' }
-          ],
-          desc: [
-            { required: true, message: '请填写活动形式', trigger: 'blur' }
-          ]
-        }
-      };
-    },
-    computed:{
-      formKey(){
-        let map = {}
-        this.formData.forEach((item)=>{
-          map[item.name] = item.value
-        })
-        return map
+  },
+  data() {
+    return {
+      ruleForm: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
       },
-      formRules(){
-        let map = {}
-        this.formData.forEach((item)=>{
-          map[item.name] = item.rules
-        })
-        return map
+      rules: {
+        name: [
+          { required: true, message: "请输入活动名称", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ],
+        region: [
+          { required: true, message: "请选择活动区域", trigger: "change" },
+        ],
+        date1: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择日期",
+            trigger: "change",
+          },
+        ],
+        date2: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择时间",
+            trigger: "change",
+          },
+        ],
+        type: [
+          {
+            type: "array",
+            required: true,
+            message: "请至少选择一个活动性质",
+            trigger: "change",
+          },
+        ],
+        resource: [
+          { required: true, message: "请选择活动资源", trigger: "change" },
+        ],
+        desc: [{ required: true, message: "请填写活动形式", trigger: "blur" }],
       },
-    },
-    methods: {
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
+      formRef: null,
+      visableFormData: {},
+    };
+  },
+  watch: {
+    formData: {
+      handler(val) {
+        console.log(111)
+        let map = {
+          properties: {},
+        };
+        for (let key in val.properties) {
+          if (val.properties[key].visable !== false) {
+            map.properties[key] = val.properties[key];
           }
+        }
+        this.$nextTick(()=>{
+          this.visableFormData = map;
         });
+        
+        //this.$forceUpdate()
       },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
+      deep:true,
+      immediate: true,
+    },
+  },
+  mounted() {
+    console.log(this);
+    this.formRef = this.$refs.ruleForm;
+  },
+  computed: {
+    hasSlot() {
+      return this.$scopedSlots.default;
+    },
+    // visableFormData(){
+    //   console.log(111)
+    //   let map = {
+    //     properties:{}
+    //   }
+    //   for(let key in this.formData.properties){
+    //     if(this.formData.properties[key].visable !== false){
+    //       map.properties[key] = this.formData.properties[key]
+    //     }
+    //   }
+    //   return map
+    // },
+    formKey() {
+      let map = {};
+      Object.keys(this.formData.properties).forEach((item) => {
+        map[item] = this.formData.properties[item].value;
+      });
+      return map;
+    },
+    formRules() {
+      let map = {};
+      Object.keys(this.formData.properties).forEach((item) => {
+        map[item] = this.formData.properties[item].rules;
+      });
+      return map;
+    },
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    makeEventCallback(eventObj, linkage) {
+      let that = this;
+      let resultObj = {};
+      if (linkage) {
+        for (let key in linkage) {
+          resultObj[key] = function (val) {
+            linkage[key].forEach((item) => {
+              console.log(val)
+              if (JSON.stringify(val) === JSON.stringify(item.equalValue)) {
+                console.log(that);
+                that.formData.properties[item.target].visable = false;
+                console.log(that.formData.properties[item.target].visable);
+                //that.$forceUpdate();
+              } else {
+                that.formData.properties[item.target].visable = true;
+              }
+            });
+          };
+        }
+        // resultObj[linkage.trigger] = function(val){
+        //   if(JSON.stringify(val) === JSON.stringify(linkage.equalValue)){
+        //     alert(111)
+        //   }
+        // }
       }
-    }
-  }
+      for (let func in eventObj) {
+        let rfunc = function (ref) {
+          return function () {
+            // 切面 做任何想做的处理
+            console.log(111);
+
+            eventObj[func](ref);
+
+            console.log(222);
+          };
+        };
+        resultObj[func] = rfunc(this.$refs.ruleForm);
+      }
+
+      return resultObj;
+    },
+  },
+};
 </script>
